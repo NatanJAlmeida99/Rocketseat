bfd3007f4c038af9c5fab50bc382504855187a76
Natan Almeida <natanalmeida158@hotmail.com>
2025-05-27 11:51:51 -0300

    finish API

--- rocketseat/03- antes_do_framework/004_APIs/main.js
+++ rocketseat/03- antes_do_framework/004_APIs/main.js
@@ -10,4 +10,31 @@ async function fetchProducts() {
     console.log(data)
 }
 
-fetchProducts()
+async function fetchProductById(id) {
+    const response = await fetch(`http://localhost:3333/products/${id}`)
+    const data = await response.json()
+    console.log(data)
+}
+
+// fetchProducts()
+// fetchProductById("3")
+
+const productName = document.querySelector("#name")
+const productPrice = document.querySelector("#price")
+const form = document.querySelector("form")
+
+addEventListener("submit", async (event) => {
+    event.preventDefault()
+
+    await fetch("http://localhost:3333/products", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id: new Date().getTime().toString(),
+            name: productName.value,
+            price: productPrice.value,
+        })
+    })
+})
